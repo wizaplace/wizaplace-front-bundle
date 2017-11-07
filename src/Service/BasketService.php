543b34fa0a952d8348e3892cef4f3840bfed49a6
hectorj <hector.jusforgues@gmail.com>
2017-11-07 12:07:17 +0100

    Save user's basket in their account.
    Also merge the basket in session with the one in the account on login.

--- src/Service/BasketService.php
+++ src/Service/BasketService.php
@@ -8,7 +8,13 @@ declare(strict_types = 1);
 namespace WizaplaceFrontBundle\Service;
 
 use GuzzleHttp\Exception\ClientException;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+use Psr\Log\NullLogger;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\AuthenticationEvents;
+use Wizaplace\SDK\Authentication\AuthenticationRequired;
 use Wizaplace\SDK\Basket\Basket;
 use Wizaplace\SDK\Basket\Comment;
 use Wizaplace\SDK\Basket\PaymentInformation;
@@ -17,7 +23,7 @@ use Wizaplace\SDK\Catalog\DeclinationId;
 /**
  * Wraps {@see \Wizaplace\SDK\Basket\BasketService}, storing the basketID for you.
  */
-class BasketService
+class BasketService implements EventSubscriberInterface
 {
     private const ID_SESSION_KEY = '_basketId';
 
@@ -30,10 +36,18 @@ class BasketService
     /** @var null|Basket */
     private $basket;
 
-    public function __construct(\Wizaplace\SDK\Basket\BasketService $baseService, SessionInterface $session)
+    /** @var LoggerInterface */
+    private $logger;
+
+    public function __construct(\Wizaplace\SDK\Basket\BasketService $baseService, SessionInterface $session, ?LoggerInterface $logger = null)
     {
         $this->baseService = $baseService;
         $this->session = $session;
+
+        if ($logger === null) {
+            $logger = new NullLogger();
+        }
+        $this->logger = $logger;
     }
 
     public function getBasket(): Basket
@@ -120,6 +134,11 @@ class BasketService
     {
         $this->basket = null;
         $this->session->remove(self::ID_SESSION_KEY);
+        try {
+            $this->baseService->setUserBasketId(null);
+        } catch (AuthenticationRequired $e) {
+            // We are not logged in, this is not a real error.
+        }
     }
 
     /**
@@ -130,19 +149,71 @@ class BasketService
         $this->baseService->updateComments($this->getBasketId(), $comments);
     }
 
+    public function onAuthenticationSuccess(): void
+    {
+        try {
+            $userBasketId = $this->baseService->getUserBasketId();
+
+            if ($userBasketId === null) {
+                $this->baseService->setUserBasketId($this->getBasketId());
+
+                return;
+            }
+
+            $currentBasketId = $this->getCurrentBasketId();
+            if (null !== $currentBasketId) {
+                $this->baseService->mergeBaskets($userBasketId, $currentBasketId);
+            }
+
+            $this->setCurrentBasketId($userBasketId);
+        } catch (\Throwable $e) {
+            // We just log the exception, we don't want this to cause an error page.
+            $this->logger->log(LogLevel::ERROR, 'Failed to load or merge the user\'s basket ID', [
+                'exception' => $e,
+            ]);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     * @uses \WizaplaceFrontBundle\Service\BasketService::onAuthenticationSuccess
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            AuthenticationEvents::AUTHENTICATION_SUCCESS => 'onAuthenticationSuccess',
+        ];
+    }
+
     /**
      * Gets current basket ID, or create a new one
      * @return string
      */
     private function getBasketId(): string
     {
-        $basketId = $this->session->get(self::ID_SESSION_KEY);
+        $basketId = $this->getCurrentBasketId();
 
         if (null === $basketId) {
             $basketId = $this->baseService->create();
-            $this->session->set(self::ID_SESSION_KEY, $basketId);
+            $this->setCurrentBasketId($basketId);
         }
 
         return $basketId;
+    }
+
+    /**
+     * Gets current basket ID, if it exists.
+     * Most of the time you should not use this , {@see \WizaplaceFrontBundle\Service\BasketService::getBasketId} instead.
+     * @return null|string
+     */
+    private function getCurrentBasketId(): ?string
+    {
+        return $this->session->get(self::ID_SESSION_KEY, null);
+    }
+
+
+    private function setCurrentBasketId(string $basketId): void
+    {
+        $this->session->set(self::ID_SESSION_KEY, $basketId);
     }
 }
