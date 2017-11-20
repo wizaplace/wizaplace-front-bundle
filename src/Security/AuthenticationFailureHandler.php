b4053b3e9a536b604ef03b6c6fd83b1dc4a18914
hectorj <hector.jusforgues@gmail.com>
2017-11-20 17:03:42 +0100

    translate authentication flash messages

--- src/Security/AuthenticationFailureHandler.php
+++ src/Security/AuthenticationFailureHandler.php
@@ -16,24 +16,29 @@ use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Http\Authentication\DefaultAuthenticationFailureHandler;
 use Symfony\Component\Security\Http\HttpUtils;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class AuthenticationFailureHandler extends DefaultAuthenticationFailureHandler
 {
     /** @var FlashBagInterface */
     private $flashBag;
 
-    public function __construct(HttpKernelInterface $httpKernel, HttpUtils $httpUtils, array $options = array(), LoggerInterface $logger = null, FlashBagInterface $flashBag)
+    /** @var TranslatorInterface */
+    private $translator;
+
+    public function __construct(HttpKernelInterface $httpKernel, HttpUtils $httpUtils, array $options = array(), LoggerInterface $logger = null, FlashBagInterface $flashBag, TranslatorInterface $translator)
     {
         $this->flashBag = $flashBag;
+        $this->translator = $translator;
         parent::__construct($httpKernel, $httpUtils, $options, $logger);
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
         if ($exception instanceof InvalidCsrfTokenException) {
-            $this->flashBag->add('warning', "L'action n'a pas pu être effectuée car elle a expirée, merci de réessayer.");
+            $this->flashBag->add('warning', $this->translator->trans('auth.login.notification.error.invalid_csrf_token'));
         } elseif ($exception instanceof BadCredentialsException) {
-            $this->flashBag->add('danger', 'Identifiants invalides, merci de réessayer.');
+            $this->flashBag->add('danger', $this->translator->trans('auth.login.notification.error.invalid_credentials'));
         }
 
         return parent::onAuthenticationFailure($request, $exception);
