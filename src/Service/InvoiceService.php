89248c8ca16b7b10c8be32efba40a06c7c3b9dff
Nicolas Gomes De Oliveira <ngdo27@gmail.com>
2017-12-01 09:18:31 +0100

    implements InvoiceService and its downloadPdf method (#13)
    * implements InvoiceService and its downloadPdf method
    
    * update CHANGELOG.md
    
    * fix lint
    
    * change content-disposition
    
    * remove unecessary annotation
    
    * fix lint
    
    * fix lint

---
+++ src/Service/InvoiceService.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @copyright   Copyright (c) Wizacha
+ * @license     Proprietary
+ */
+declare(strict_types = 1);
+
+
+namespace WizaplaceFrontBundle\Service;
+
+use Symfony\Component\HttpFoundation\Response;
+use Wizaplace\SDK\Order\OrderService;
+
+class InvoiceService
+{
+    /**
+     * @var OrderService
+     */
+    private $orderService;
+
+    public function __construct(OrderService $orderService)
+    {
+        $this->orderService = $orderService;
+    }
+
+    public function downloadPdf(int $orderId, string $filename = "Invoice"): Response
+    {
+        $pdf = $this->orderService->downloadPdfInvoice($orderId);
+
+        $response = new Response($pdf->getContents());
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->headers->set('Content-Disposition', 'attachment; filename: "'.$filename.'.pdf"');
+
+        return $response;
+    }
+}
