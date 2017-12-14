9174ebb887341953feb447948888517105199fa7
Nicolas Gomes <nicolas@wizaplace.com>
2017-12-14 09:20:09 +0100

    add \WizaplaceFrontBundle\Service\DiscussionService::contact

--- src/Service/DiscussionService.php
+++ src/Service/DiscussionService.php
@@ -8,6 +8,7 @@ declare(strict_types = 1);
 
 namespace WizaplaceFrontBundle\Service;
 
+use Symfony\Bridge\Twig\TwigEngine;
 use \Wizaplace\SDK\Discussion\DiscussionService as BaseService;
 
 class DiscussionService
@@ -22,33 +23,24 @@ class DiscussionService
      */
     private $environment;
 
-    public function __construct(BaseService $baseService, \Twig_Environment $environment)
+    public function __construct(BaseService $baseService, TwigEngine $environment)
     {
         $this->discussionService = $baseService;
         $this->environment = $environment;
-        $loader = new \Twig_Loader_Filesystem('src/Resources/views');
-        $this->environment->setLoader($loader);
     }
 
-    /**
-     * @throws \Twig_Error_Loader
-     * @throws \Twig_Error_Runtime
-     * @throws \Twig_Error_Syntax
-     */
     public function contact(
         string $email,
         string $subject,
         string $message,
         array $extraData = [],
-        string $templateName = 'contact_template.html.twig'
-    ) : void
+        string $templateName = '@WizaplaceFront/contact_template.html.twig'
+    ): void
     {
         $message = $this->environment->render($templateName, [
             'extraData' => $extraData,
             'message' => $message,
         ]);
-
-        var_dump($message);
 
         $this->discussionService->submitContactRequest($email, $subject, $message);
     }
--- tests/Service/DiscussionServiceTest.php
+++ tests/Service/DiscussionServiceTest.php
@@ -8,7 +8,6 @@ declare(strict_types = 1);
 
 namespace WizaplaceFrontBundle\Tests\Service;
 
-use Wizaplace\SDK\ApiClient;
 use WizaplaceFrontBundle\Service\DiscussionService;
 use WizaplaceFrontBundle\Tests\BundleTestCase;
 
@@ -17,16 +16,14 @@ class DiscussionServiceTest extends BundleTestCase
     public function testContact()
     {
         $container = self::$kernel->getContainer();
-        $apiClient = $container->get(ApiClient::class);
 
-        $baseService = new \Wizaplace\SDK\Discussion\DiscussionService($apiClient);
-        $loader = new \Twig_Loader_Filesystem();
-        $twigEnvironment = new \Twig_Environment($loader, []);
-        $discussionService = new DiscussionService($baseService, $twigEnvironment);
+        $discussionService = $container->get(DiscussionService::class);
 
         $discussionService->contact('john.doe@email.com', 'I have a request', 'I would like to contact an admin', [
             'firstName' => 'John',
             'lastName' => 'Doe',
         ]);
+
+        $this->assertTrue(true);
     }
 }
