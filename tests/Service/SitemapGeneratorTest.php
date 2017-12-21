9d12a5168dde6cfb8ff8938299833fa76e0e8a76
Nicolas Gomes De Oliveira <ngdo27@gmail.com>
2017-12-21 10:19:34 +0100

    Add new SitemapGenerator
    Update changelog
    
    Sitemap: add static URLs, and improve test

---
+++ tests/Service/SitemapGeneratorTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @copyright Copyright (c) Wizacha
+ * @license Proprietary
+ */
+declare(strict_types=1);
+
+namespace WizaplaceFrontBundle\Tests\Service;
+
+use SitemapGenerator\Sitemap\Sitemap;
+use WizaplaceFrontBundle\Tests\BundleTestCase;
+
+class SitemapGeneratorTest extends BundleTestCase
+{
+    public function testGenerate()
+    {
+        $sitemapGenerator = self::$kernel->getContainer()->get('test.WizaplaceFrontBundle\Service\SitemapGenerator');
+
+        /** @var \PHPUnit_Framework_MockObject_MockObject|Sitemap $sitemap */
+        $sitemap = $this->createMock(Sitemap::class);
+
+        $spy = self::exactly(29);
+        $sitemap->expects($spy)->method('add');
+
+        $sitemapGenerator->populate($sitemap);
+
+        /** @var string[] $urls */
+        $urls = array_map(function (\PHPUnit_Framework_MockObject_Invocation_Static $invocation): string {
+            return $invocation->parameters[0]->getLoc();
+        }, $spy->getInvocations());
+
+        self::assertContains('http://localhost/', $urls); // static URL
+        self::assertContains('http://localhost/a/adidas', $urls); // attribute variant URL
+        self::assertContains('http://localhost/p/it/headsets/casque-corsair-gaming', $urls); // product URL
+        self::assertContains('http://localhost/c/categorie-principale', $urls); // category URL
+        self::assertContains('http://localhost/v/acme', $urls); // company URL
+        self::assertContains('http://localhost/faq', $urls); // CMS page URL
+    }
+}
