0d2f9cc2a21f8880a4680b5d24ea553d215dee10
Hector Jusforgues <hector.jusforgues@gmail.com>
2018-02-19 15:01:47 +0100

    Forward getters from Security User to the SDK's User (#36)
    * Forward getters from Security User to the SDK's User
    
    * update changelog

--- src/Security/User.php
+++ src/Security/User.php
@@ -10,7 +10,9 @@ namespace WizaplaceFrontBundle\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Wizaplace\SDK\Authentication\ApiKey;
 use Wizaplace\SDK\User\User as WizaplaceUser;
+use Wizaplace\SDK\User\UserAddress;
 use Wizaplace\SDK\User\UserService;
+use Wizaplace\SDK\User\UserTitle;
 
 class User implements UserInterface, \Serializable
 {
@@ -72,15 +74,6 @@ class User implements UserInterface, \Serializable
     {
     }
 
-    public function getWizaplaceUser(): WizaplaceUser
-    {
-        if (!$this->userIsFresh) {
-            $this->refreshWizaplaceUser();
-        }
-
-        return $this->wizaplaceUser;
-    }
-
     public function getApiKey(): ApiKey
     {
         return $this->apiKey;
@@ -100,6 +93,95 @@ class User implements UserInterface, \Serializable
         $this->apiKey = $data['apiKey'];
         $this->wizaplaceUser = $data['wUser'];
         $this->userIsFresh = false;
+    }
+
+    /**
+     * @see \Wizaplace\SDK\User\User::getId
+     */
+    public function getId(): int
+    {
+        return $this->wizaplaceUser->getId();
+    }
+
+    /**
+     * @see \Wizaplace\SDK\User\User::getEmail
+     */
+    public function getEmail(): string
+    {
+        return $this->wizaplaceUser->getEmail();
+    }
+
+    /**
+     * @see \Wizaplace\SDK\User\User::getTitle
+     */
+    public function getTitle(): ?UserTitle
+    {
+        return $this->wizaplaceUser->getTitle();
+    }
+
+    /**
+     * @see \Wizaplace\SDK\User\User::getFirstname
+     */
+    public function getFirstname(): string
+    {
+        return $this->wizaplaceUser->getFirstname();
+    }
+
+    /**
+     * @see \Wizaplace\SDK\User\User::getLastname
+     */
+    public function getLastname(): string
+    {
+        return $this->wizaplaceUser->getLastname();
+    }
+
+    /**
+     * @see \Wizaplace\SDK\User\User::getBirthday
+     */
+    public function getBirthday(): ?\DateTimeImmutable
+    {
+        return $this->wizaplaceUser->getBirthday();
+    }
+
+    /**
+     * @see \Wizaplace\SDK\User\User::getBillingAddress
+     */
+    public function getBillingAddress(): ?UserAddress
+    {
+        return $this->wizaplaceUser->getBillingAddress();
+    }
+
+    /**
+     * @see \Wizaplace\SDK\User\User::getShippingAddress
+     */
+    public function getShippingAddress(): ?UserAddress
+    {
+        return $this->wizaplaceUser->getShippingAddress();
+    }
+
+    /**
+     * @see \Wizaplace\SDK\User\User::getCompanyId
+     */
+    public function getCompanyId(): ?int
+    {
+        return $this->wizaplaceUser->getCompanyId();
+    }
+
+    /**
+     * @see \Wizaplace\SDK\User\User::isVendor
+     */
+    public function isVendor(): bool
+    {
+        return $this->wizaplaceUser->isVendor();
+    }
+
+    public function getWizaplaceUser(): WizaplaceUser
+    {
+        if (!$this->userIsFresh) {
+            $this->refreshWizaplaceUser();
+        }
+
+        return $this->wizaplaceUser;
     }
 
     private function refreshWizaplaceUser(): void
