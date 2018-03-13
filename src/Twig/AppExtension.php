e40bd869d65e6bc220a5239684cb57d16214f889
Nicolas Gomes De Oliveira <ngdo27@gmail.com>
2018-03-13 11:52:58 +0100

    fix lint

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -121,10 +121,7 @@ class AppExtension extends \Twig_Extension
         $integerPart = floor($formattedPrice);
         $decimalPart = $formattedPrice - $integerPart;
 
-        return  '<span class="price__integer-part">'.$integerPart.'</span>'.
-                '<span class="price__delimiter">'.$this->translator->trans('price.delimiter').'</span>'.
-                '<span class="price__decimal-part">'.$decimalPart.'</span>'.
-                '<span class="price__currency">'.$this->translator->trans('price.currency').'</span>';
+        return '<span class="price__integer-part">'.$integerPart.'</span>'.'<span class="price__delimiter">'.$this->translator->trans('price.delimiter').'</span>'.'<span class="price__decimal-part">'.$decimalPart.'</span>'.'<span class="price__currency">'.$this->translator->trans('price.currency').'</span>';
     }
 
     /**
