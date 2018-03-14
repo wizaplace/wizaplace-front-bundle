77e83aa45307f79bbae9fdea92315e68d01ae894
hectorj <hector.jusforgues@gmail.com>
2018-03-14 09:30:34 +0100

    fix decimal delimiter, and add thousands delimiter

--- tests/Service/AppExtensionTest.php
+++ tests/Service/AppExtensionTest.php
@@ -16,14 +16,14 @@ class AppExtensionTest extends BundleTestCase
         $container = self::$kernel->getContainer();
         $extension = $container->get('test.WizaplaceFrontBundle\Twig\AppExtension');
 
-        $this->assertSame('<span class="price__integer-part">0</span><span class="price__delimiter">price.delimiter</span><span class="price__decimal-part">00</span><span class="price__currency"> €</span>', $extension->formatPrice(0.));
+        $this->assertSame('<span class="price__integer-part">0</span><span class="price__delimiter">,</span><span class="price__decimal-part">00</span><span class="price__currency"> €</span>', $extension->formatPrice(0.));
 
-        $this->assertSame('<span class="price__integer-part">1</span><span class="price__delimiter">price.delimiter</span><span class="price__decimal-part">00</span><span class="price__currency"> €</span>', $extension->formatPrice(1.));
+        $this->assertSame('<span class="price__integer-part">1</span><span class="price__delimiter">,</span><span class="price__decimal-part">00</span><span class="price__currency"> €</span>', $extension->formatPrice(1.));
 
-        $this->assertSame('<span class="price__integer-part">3</span><span class="price__delimiter">price.delimiter</span><span class="price__decimal-part">14</span><span class="price__currency"> €</span>', $extension->formatPrice(3.14159265359));
+        $this->assertSame('<span class="price__integer-part">3</span><span class="price__delimiter">,</span><span class="price__decimal-part">14</span><span class="price__currency"> €</span>', $extension->formatPrice(3.14159265359));
 
-        $this->assertSame('<span class="price__integer-part">3</span><span class="price__delimiter">price.delimiter</span><span class="price__decimal-part">50</span><span class="price__currency"> €</span>', $extension->formatPrice(3.5));
+        $this->assertSame('<span class="price__integer-part">3</span><span class="price__delimiter">,</span><span class="price__decimal-part">50</span><span class="price__currency"> €</span>', $extension->formatPrice(3.5));
 
-        $this->assertSame('<span class="price__integer-part">1,000</span><span class="price__delimiter">price.delimiter</span><span class="price__decimal-part">10</span><span class="price__currency"> €</span>', $extension->formatPrice(1000.1));
+        $this->assertSame('<span class="price__integer-part">1 000</span><span class="price__delimiter">,</span><span class="price__decimal-part">10</span><span class="price__currency"> €</span>', $extension->formatPrice(1000.1));
     }
 }
