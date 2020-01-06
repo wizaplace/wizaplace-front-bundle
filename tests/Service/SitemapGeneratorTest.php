d95b440709c445da36950ca5011c93d6e133b045
Dragoon1010 <jonathan.renard@wizacha.com>
2020-01-06 14:35:50 +0100

    add base of test unit

--- tests/Service/SitemapGeneratorTest.php
+++ tests/Service/SitemapGeneratorTest.php
@@ -9,6 +9,7 @@ namespace WizaplaceFrontBundle\Tests\Service;
 
 use PHPUnit\Framework\MockObject\Invocation\ObjectInvocation;
 use SitemapGenerator\Sitemap\Sitemap;
+use WizaplaceFrontBundle\Service\SitemapGenerator;
 use WizaplaceFrontBundle\Tests\BundleTestCase;
 
 class SitemapGeneratorTest extends BundleTestCase
@@ -36,5 +37,25 @@ class SitemapGeneratorTest extends BundleTestCase
         self::assertContains('http://localhost/c/categorie-principale', $urls); // category URL
         self::assertContains('http://localhost/v/acme', $urls); // company URL
         self::assertContains('http://localhost/faq', $urls); // CMS page URL
+    }
+
+    public function testGenerateMultilangual() : void
+    {
+        $sitemapGenerator = self::$kernel->getContainer()->get('test.Multilanguage.WizaplaceFrontBundle\Service\SitemapGenerator');
+
+        /** @var \PHPUnit_Framework_MockObject_MockObject|Sitemap $sitemap */
+        $sitemap = $this->createMock(Sitemap::class);
+
+        $spy = self::exactly(29);
+        $sitemap->expects($spy)->method('add');
+
+        $sitemapGenerator->populate($sitemap);
+
+        /** @var string[] $urls */
+        $urls = array_map(function (ObjectInvocation $invocation): string {
+            return $invocation->getParameters()[0]->getLoc();
+        }, $spy->getInvocations());
+
+        var_dump($urls);
     }
 }
